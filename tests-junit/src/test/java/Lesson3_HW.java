60098e9892d5818c3d50be3b35e83b6ca823cc82
Kinchop <kinchop@yandex.ru>
2021-04-11 14:06:40 +0400

    test added

---
+++ tests-junit/src/test/java/Lesson3_HW.java
@@ -0,0 +1,46 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Lesson3_HW {
+
+    private Logger logger = LogManager.getLogger(Lesson3_HW.class);
+    protected static WebDriver driver;
+    private TestConfig config;
+
+
+    @Before
+    public void StartUp() {
+        config = ConfigFactory.create(TestConfig.class);
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("Драйвер поднят");
+    }
+
+    @After
+    public void End() {
+        if (driver != null)
+            driver.quit();
+        logger.info("Драйвер закрыт");
+    }
+
+    @Test
+    public void WebDriverTest() {
+
+        driver.get(config.hostname());
+        logger.info("Сайт открыт");
+        String actual = driver.getTitle();
+        Assert.assertEquals(config.title(), actual);
+
+
+    }
+
+}
