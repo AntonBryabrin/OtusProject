7f14fada7162b6f393b27dfd105ac0ffad9b6824
Kinchop <kinchop@yandex.ru>
2021-04-15 19:26:25 +0400

    delete default values in TestConfig

--- tests-junit/src/test/java/TestConfig.java
+++ tests-junit/src/test/java/TestConfig.java
@@ -6,10 +6,10 @@ import java.net.URL;
 @Config.Sources({"classpath:general.properties"})
 public interface TestConfig extends Config {
     @Config.Key("host")
-    @DefaultValue("https://otus.ru")
+    @DefaultValue("")
     String hostname();
 
-    @DefaultValue("Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям")
+    @DefaultValue("")
     String title();
 
 }
