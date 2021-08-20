0609e20a1137f6bfa687d841fcf03ddafeca28d6
Anton Bryabrin <anton.bryabrin@piano.io>
2021-08-20 13:04:50 +0400

    added screnshots

--- tests-junit/src/test/java/Lesson12_HW/cases/Lesson12_HW_Test.java
+++ tests-junit/src/test/java/Lesson12_HW/cases/Lesson12_HW_Test.java
@@ -1,6 +1,7 @@
 package Lesson12_HW.cases;
 
 
+import io.qameta.allure.*;
 import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,6 +11,10 @@ import org.junit.Test;
 import configuration.TestConfig;
 import Lesson12_HW.pages.*;
 import Lesson12_HW.utils.BaseHooks;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.ByteArrayInputStream;
 
 public class Lesson12_HW_Test extends BaseHooks{
     private Logger logger = LogManager.getLogger(Lesson12_HW_Test.class);
@@ -21,41 +26,38 @@ public class Lesson12_HW_Test extends BaseHooks{
 
 
     @Test
+    @Epic("Otus")
+    @Feature("Fill info")
+    @Story("Filling info about user")
+    @Description("Check that filled info is displayed after reopening page")
+
     public void otusPersonalInfoCheck(){
-       // WebDriverWait wait = new WebDriverWait(driver, 25);
+
         config = ConfigFactory.create(TestConfig.class);
         otusLoginPage loginPage = new otusLoginPage(driver);
 
-
         loginPage.open();
         logger.info("Step 1 Открыта страница логина");
-
         loginPage.login(config.loginOtus(), config.passwordOtus());
         logger.info("Step 2 Пользователь авторизован");
-
         otusCabinetPage cabinetPage = new otusCabinetPage(driver);
         cabinetPage.open();
         logger.info("Step 3 Открыта личная информация");
-
         cabinetPage.fillInfo();
         logger.info("Step 4 Личная информация заполнена");
-
+        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
 
         BaseHooks.restartDriver();
         logger.info("Перезапуск браузера");
-
         otusLoginPage loginPageRestarted = new otusLoginPage(driver);
         loginPageRestarted.open();
         logger.info("Step 5 Открыта страница логина");
-
         loginPageRestarted.login(config.loginOtus(), config.passwordOtus());
         logger.info("Step 6 Пользователь авторизован");
-
         otusCabinetPage cabinetPageRestarted = new otusCabinetPage(driver);
         cabinetPageRestarted.open();
         logger.info("Step 7 Открыта личная информация");
-
-        //Thread.sleep(75000);
+        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
 
         softly.assertThat(cabinetPageRestarted.getUserName()).as("Compare name").isEqualTo(config.usernameOtus());
         softly.assertThat(cabinetPageRestarted.getUserNameLat()).as("Compare latin name").isEqualTo(config.usernameLatOtus());
